b85323f59f273d219b6f015ed71092e8952fdc30
RainbowSoul <rainbow-life@outlook.fr>
2020-09-02 19:35:17 +0200

    Back Office update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,12 +1,33 @@
 <?php
     namespace App\Controller;
+    use App\Entity\Category;
+    use App\Entity\User;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     class AdminController extends AbstractController {
         /**
-         * @Route("/admin", name="admin_backoffice", methods={"GET"})
+         * @IsGranted("ROLE_ADMIN")
+         * @Route("/admin", name="admin_backOffice", methods={"GET"})
+         * @return Response
          */
-        public function backoffice() {
-            return $this->render('admin/backoffice.html.twig');
+        public function backOffice() {
+            $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+            $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+            return $this->render('admin/backoffice.html.twig', ['users' => $users, 'categories' => $categories]);
+        }
+        /**
+         * @IsGranted("ROLE_ADMIN")
+         * @Route("/utilisateur/supprimer/{id}", name="admin_deleteUser", methods={"GET"})
+         * @param $id
+         * @return Response
+         */
+        public function deleteUser($id) {
+            $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin_backOffice');
         }
     }
