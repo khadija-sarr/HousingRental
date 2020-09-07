00eb25182b1d553a37a9d92b9e931f71588ad715
RainbowSoul <rainbow-life@outlook.fr>
2020-09-07 21:26:45 +0200

    Updating files + Form page's front-end + Project folder structure

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -37,7 +37,7 @@
                 ->add('zipcode', IntegerType::class, ['label' => false, 'attr' => ['placeholder' => "Code Postal"]])
                 ->add('country', CountryType::class, ['label' => false])
                 ->add('photo', FileType::class, ['label' => false, 'attr' => ['class' => 'dropify']])
-                ->add('submit', SubmitType::class, ['label' => 'Valider mon inscription'])
+                ->add('submit', SubmitType::class, ['label' => 'Valider'])
                 ->getForm();
             $form->handleRequest($request);
             if($form->isSubmitted() && $form->isValid()) {
