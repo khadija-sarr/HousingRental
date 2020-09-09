4e489c1fed19ea5b420f7ecd1d0c98e0ea049239
harold venere <harold.venere@gmail.com>
2020-09-09 19:12:25 +0200

    final card

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,7 +6,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/connexion", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -22,7 +22,7 @@ class SecurityController extends AbstractController {
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/deconnexion", name="app_logout")
      */
     public function logout() {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
