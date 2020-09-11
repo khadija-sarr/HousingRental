55eb806039ed9da562ff9c189b04d18f8c912cbd
harold venere <harold.venere@gmail.com>
2020-09-11 09:51:02 +0200

    profil et edit profil

--- src/Entity/House.php
+++ src/Entity/House.php
@@ -171,17 +171,6 @@ class House
         return $this;
     }
 
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    public function setAddress(string $address): self
-    {
-        $this->address = $address;
-
-        return $this;
-    }
 
     public function getUser(): ?User
     {
